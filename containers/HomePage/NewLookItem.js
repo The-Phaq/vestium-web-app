324023592760dca2f1c98a4841bbd9b3c5594c46
QuangHD <quang.hd0102@gmail.com>
2021-07-07 17:35:07 +0700

    Update data in new look page with data getting from API

--- containers/HomePage/NewLookItem.js
+++ containers/HomePage/NewLookItem.js
@@ -1,43 +1,41 @@
-import React from 'react';
-import { Avatar, Col, Button, Divider, Row } from 'antd';
-import Image from 'next/image';
-import {
-  HeartIcon,
-} from 'components/SVGIcon';
-import { UserOutlined } from '@ant-design/icons';
-import { NewLookItemWrapper } from './styles';
+import React from "react";
+import { Avatar, Col, Button, Divider, Row } from "antd";
+import Image from "next/image";
+import { HeartIcon } from "components/SVGIcon";
+import { UserOutlined } from "@ant-design/icons";
+import { NewLookItemWrapper } from "./styles";
 
 const infos = [
   {
-    id: 'votes',
-    shape: 'round',
-    text: 'VOTE',
-    value: data => `${data} Votes`,
+    id: "votes",
+    shape: "round",
+    text: "VOTE",
+    value: (data) => `${data} Votes`,
     isPrimary: true,
   },
   {
-    id: 'favorite',
-    shape: 'circle',
+    id: "favorite",
+    shape: "circle",
     Icon: HeartIcon,
-    value: () => 'Favorite',
+    value: () => "Favorite",
   },
   {
-    id: 'shares',
-    shape: 'round',
-    text: 'SHARE',
-    value: data => `${data} Shares`,
+    id: "shares",
+    shape: "round",
+    text: "SHARE",
+    value: (data) => `${data} Shares`,
   },
   {
-    id: 'followers',
-    shape: 'round',
-    text: 'FOLLOW',
-    value: data => `${data} Followers`,
+    id: "followers",
+    shape: "round",
+    text: "FOLLOW",
+    value: (data) => `${data} Followers`,
   },
-]
+];
 
 const NewLookItem = ({ newLook }) => {
   const { img, user, name, tags, items } = newLook || {};
-
+  console.log(newLook);
   return (
     <NewLookItemWrapper gutter={[20, 20]}>
       <Col md={11} sm={24} xs={24}>
@@ -50,23 +48,21 @@ const NewLookItem = ({ newLook }) => {
           <div className="user-section">
             <div className="user">
               <Avatar src={user?.avatar} icon={<UserOutlined />} size={44} />
-              <div className="name">
-                {user?.name}
-              </div>
+              <div className="name">{`${user?.firstName} ${user?.lastName}`}</div>
             </div>
             <div className="info">
-              {infos.map(info => (
+              {infos.map((info) => (
                 <div className="info-button" key={info?.id}>
                   <Button
                     shape={info?.shape}
-                    {...info?.isPrimary && {
-                      type: 'primary',
-                    }}
-                    {...info?.Icon && {
+                    {...(info?.isPrimary && {
+                      type: "primary",
+                    })}
+                    {...(info?.Icon && {
                       icon: <info.Icon />,
-                    }}
+                    })}
                   >
-                    {info?.text || ''}
+                    {info?.text || ""}
                   </Button>
                   <div className="info-value">
                     {info?.value(newLook?.[info?.id])}
@@ -77,39 +73,37 @@ const NewLookItem = ({ newLook }) => {
           </div>
           <Divider />
           <div className="item-section">
-            <div className="item-title">
-              {name}
-            </div>
+            <div className="item-title">{name}</div>
             <div className="tags">
-              {tags?.toString()?.replaceAll(',', '  ·  ')}
+              {tags?.toString()?.replaceAll(",", "  ·  ")}
             </div>
             {items?.length > 0 && (
               <Row gutter={[20, 20]} className="items">
-                {items.map(({ id, name: itemName, brandName, price, img: itemImg }) => (
-                  <Col span={8} key={id}>
-                    <div className="item-wrapper">
-                      <div className="item-image">
-                        <Image objectFit="contain" layout="fill" src={itemImg} />
+                {items.map(
+                  ({ id, name: itemName, brandName, price, img: itemImg }) => (
+                    <Col span={8} key={id}>
+                      <div className="item-wrapper">
+                        <div className="item-image">
+                          <Image
+                            objectFit="contain"
+                            layout="fill"
+                            src={itemImg}
+                          />
+                        </div>
+                        <div className="item-name">{itemName}</div>
+                        <div className="item-name">{brandName}</div>
+                        <div className="price">{price}</div>
                       </div>
-                      <div className="item-name">
-                        {itemName}
-                      </div>
-                      <div className="item-name">
-                        {brandName}
-                      </div>
-                      <div className="price">
-                        {price}
-                      </div>
-                    </div>
-                  </Col>
-                ))}
+                    </Col>
+                  )
+                )}
               </Row>
             )}
           </div>
         </div>
       </Col>
     </NewLookItemWrapper>
-  )
-}
+  );
+};
 
 export default NewLookItem;
--- store/newlooks/selectors.js
+++ store/newlooks/selectors.js
@@ -1,36 +1,41 @@
-import { createSelector } from 'reselect';
-import { CRUDSelectors } from 'store/crudCreator/selectors';
-import { MODEL_NAME } from './actions';
+import { createSelector } from "reselect";
+import { CRUDSelectors } from "store/crudCreator/selectors";
+import { MODEL_NAME } from "./actions";
 
 export const newlooksSelectors = new CRUDSelectors(MODEL_NAME);
 
-const getNewLooks = state => state.newlooks.data;
-const getNewLookIds = state => state.newlooks.ids;
+const getNewLooks = (state) => state.newlooks.data;
+const getNewLookIds = (state) => state.newlooks.ids;
 
 export const getNewLooksSelectors = createSelector(
   [getNewLooks, getNewLookIds],
   (newLookData, newLookIds) => {
-    return newLookIds.map(id => ({
+    return newLookIds.map((id) => ({
       ...newLookData[id],
       id: newLookData[id]?._id,
-      img: newLookData[id]?.image?.url || '/images/newLook.png',
-      user: {
-        name: 'Anne Giao',
-        avatar: '/images/avatar.png',
-      },
+      img: newLookData[id]?.url || "/images/newLook.png",
       votes: 214,
       shares: 46,
       followers: 132,
-      tags: ['office', 'everyDay', 'casual', 'sporty', 'black', 'spring', 'summer', 'medium'],
-      items: newLookData[id]?.items?.map(item => ({
+      tags: [
+        "office",
+        "everyDay",
+        "casual",
+        "sporty",
+        "black",
+        "spring",
+        "summer",
+        "medium",
+      ],
+      items: newLookData[id]?.items?.map((item) => ({
         ...item,
         id: item?.itemId?._id,
         _id: item?.itemId?._id,
         name: item?.itemId?.name,
-        brandName: 'Gucci',
+        brandName: "Gucci",
         price: item?.itemId?.price,
         img: item?.url || item?.itemId?.image?.url,
       })),
     }));
-  },
-)
+  }
+);
