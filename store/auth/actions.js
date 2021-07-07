64ec50fc6fc82f93ed7417c875f63327330f6a8b
QuangHD <quang.hd0102@gmail.com>
2021-07-07 22:05:07 +0700

    Remove logout API

--- store/auth/actions.js
+++ store/auth/actions.js
@@ -1,77 +1,77 @@
-import { message } from 'antd';
+import { message } from "antd";
 import {
-    fetchLogin,
-    fetchLogout,
-    fetchRegister,
-    fetchProfile,
-} from '../../api/user';
-import slice from './index';
+  fetchLogin,
+  fetchLogout,
+  fetchRegister,
+  fetchProfile,
+} from "../../api/user";
+import slice from "./index";
 
 const { loginSuccess, logoutSuccess, loginFetch, loginFailed, profileSuccess } =
-    slice.actions;
+  slice.actions;
 export const login =
-    ({ username, password }) =>
-    async (dispatch) => {
-        dispatch(loginFetch());
-        try {
-            const res = await fetchLogin({ email: username, password });
-            dispatch(loginSuccess(res));
-        } catch (e) {
-            message.error(e.message);
-            dispatch(loginFailed());
-            return console.error(e.message);
-        }
-    };
-export const registerAction = (data) => async (dispatch) => {
+  ({ username, password }) =>
+  async (dispatch) => {
     dispatch(loginFetch());
     try {
-        const res = await register(data);
-        dispatch(loginSuccess(res));
+      const res = await fetchLogin({ email: username, password });
+      dispatch(loginSuccess(res));
     } catch (e) {
-        message.error(e.message);
-        dispatch(loginFailed());
-        return console.error(e.message);
+      message.error(e.message);
+      dispatch(loginFailed());
+      return console.error(e.message);
     }
+  };
+export const registerAction = (data) => async (dispatch) => {
+  dispatch(loginFetch());
+  try {
+    const res = await register(data);
+    dispatch(loginSuccess(res));
+  } catch (e) {
+    message.error(e.message);
+    dispatch(loginFailed());
+    return console.error(e.message);
+  }
 };
 export const logout = () => async (dispatch) => {
-    try {
-        const res = await fetchLogout();
-        return dispatch(logoutSuccess());
-    } catch (e) {
-        return console.error(e.message);
-    }
+  try {
+    // fetchLogout();
+    return dispatch(logoutSuccess());
+  } catch (e) {
+    return console.error(e.message);
+  }
 };
 
 export const register =
-    ({ name, email, password, ...query }) =>
-    async (dispatch) => {
-        dispatch(loginFetch());
-        try {
-            const res = await fetchRegister({
-                firstName: name,
-                lastName: name,
-                email,
-                password,
-                ...query,
-            });
-            message.success('Your account have bên create successfully!');
-            dispatch(loginSuccess(res));
-        } catch (e) {
-            message.error(e.message);
-            dispatch(loginFailed());
-            return console.error(e.message);
-        }
-    };
-
-export const getProfile = () => async (dispatch) => {
+  ({ name, email, password, ...query }) =>
+  async (dispatch) => {
     dispatch(loginFetch());
     try {
-        const res = await fetchProfile();
-        console.log('resProfile', res);
-        dispatch(profileSuccess(res));
+      const res = await fetchRegister({
+        firstName: name,
+        lastName: name,
+        email,
+        password,
+        ...query,
+      });
+      message.success("Your account have bên create successfully!");
+      dispatch(loginSuccess(res));
     } catch (e) {
-        // message.error(e.message);
-        dispatch(loginFailed());
-        return console.error(e.message);
+      message.error(e.message);
+      dispatch(loginFailed());
+      return console.error(e.message);
     }
+  };
+
+export const getProfile = () => async (dispatch) => {
+  dispatch(loginFetch());
+  try {
+    const res = await fetchProfile();
+    console.log("resProfile", res);
+    dispatch(profileSuccess(res));
+  } catch (e) {
+    // message.error(e.message);
+    dispatch(loginFailed());
+    return console.error(e.message);
+  }
 };
