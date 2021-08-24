0bac219a88a3674e9ab5a4031bab0f454b1bd089
QuangHD <quang.hd0102@gmail.com>
2021-08-24 20:12:53 +0700

    move 'locales' folder to public

--- i18n.js
+++ i18n.js
@@ -1,13 +1,15 @@
-import * as path from 'path';
-import NextI18next from 'next-i18next';
-import cookies from 'js-cookie';
+import * as path from "path";
+import NextI18next from "next-i18next";
+import cookies from "js-cookie";
 
 export const nextI18next = new NextI18next({
-  defaultLanguage: cookies.get('next-i18next') || 'en',
-  defaultNS: 'common',
-  fallbackLng: cookies.get('next-i18next') || 'en',
-  localePath: path.resolve('./locales'),
-  otherLanguages: ['fr', 'en', 'vi'].filter(lang => lang !== (cookies.get('next-i18next') || 'en')),
+  defaultLanguage: cookies.get("next-i18next") || "en",
+  defaultNS: "common",
+  fallbackLng: cookies.get("next-i18next") || "en",
+  localePath: path.resolve("./public/locales"),
+  otherLanguages: ["fr", "en", "vi"].filter(
+    (lang) => lang !== (cookies.get("next-i18next") || "en")
+  ),
   interpolation: {
     escapeValue: false,
   },
@@ -21,4 +23,3 @@ export const {
   withTranslation,
   i18n,
 } = nextI18next;
-
