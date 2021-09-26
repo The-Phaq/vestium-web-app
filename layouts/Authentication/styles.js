88e92986ab8f10e8b1375d0b624a9e4b9a896a34
phuc.le11 <phuc.le@enouvo.com>
2021-09-26 09:48:18 +0700

    update auth background

--- layouts/Authentication/styles.js
+++ layouts/Authentication/styles.js
@@ -6,7 +6,7 @@ export default styled(Layout)`
 
   .content-wrapper {
     height: 100vh;
-    background-image: url("/images/bgimage.png");
+    background-image: url("/images/background.png");
     background-repeat: no-repeat;
     background-size: cover;
     position: relative;
